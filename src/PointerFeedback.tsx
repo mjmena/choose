1fd4c1469c607251300252f5278ab4c1a30cd735
Martin Mena <mjmena.dev@gmail.com>
2019-02-11 21:33:17 -0500

    Allow pointer feedback to slowly expand and deflate
    Use two different types of springs to handle movement and pulse
    Currently animations are out of sync if spawned on different times

--- src/PointerFeedback.tsx
+++ src/PointerFeedback.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import randomColor from "randomcolor";
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated, config } from "react-spring";
 
 import Pointer from "./types/Pointer";
 
@@ -8,8 +8,37 @@ type Props = {
   pointer: Pointer;
 };
 
+function useInterval(callback: () => void) {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    let id = setInterval(tick, 1500);
+    return () => clearInterval(id);
+  }, []);
+}
+
 const PointerFeedback = ({ pointer }: Props) => {
   const [color] = useState(randomColor());
+  const [expand, setExpand] = useState(false);
+
+  const toggleExpand = useCallback(() => {
+    setExpand(lastExpand => !lastExpand);
+  }, []);
+
+  useInterval(toggleExpand);
+
+  const spawn = useSpring({
+    to: { r: expand ? 65 : 45 },
+    config: config.molasses
+  });
 
   const props = useSpring({
     from: { opacity: 0 },
@@ -20,10 +49,11 @@ const PointerFeedback = ({ pointer }: Props) => {
       cx: pointer.x,
       cy: pointer.y
     },
-    config: { mass: 0, tension: 0, friction: 1 }
+    config: config.stiff
   });
 
-  return <animated.circle r={50} style={props} />;
+  //@ts-ignore
+  return <animated.circle r={spawn.r} style={props} />;
 };
 
 export default React.memo(PointerFeedback);
