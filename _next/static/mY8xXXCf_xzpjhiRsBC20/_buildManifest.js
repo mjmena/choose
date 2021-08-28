2d9c571b41bfec29962300f72269f8becf7b50f9
mjmena <martinjxm@gmail.com>
2021-08-28 15:32:58 +0000

    Deploying to gh-pages from @ mjmena/choose@640d463efbfa7d1808f9a928c288ba541652ce1b 🚀

---
+++ _next/static/mY8xXXCf_xzpjhiRsBC20/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/524-235a69c42d967b162d8e.js","static/chunks/pages/index-adc685e743cce00f789b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
